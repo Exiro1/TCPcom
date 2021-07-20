f2205004e78d7a1d313be841d2bf887448b9a8eb
Corentin Generet <c.generet123@gmail.com>
2021-07-20 20:04:39 +0200

    ajout ecran

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import time
 from random import random
+from tkinter import Tk
 
 from TCPcom import TCPCOM, encode_data
 import numpy as np
 
+from dashboard import Dashboard
+
 
 def start():
     c = TCPCOM()
+    window = Tk()
+    dash = Dashboard(window,rand_data())
 
     data = np.array(
         [12000, 56.25, -356, 1003, 23.56, 23.89, 6687, 32.56, -1200, 68.72, 48.63, 2963, 0, 1, 0, 1, 0, 1, 0, 1,
          0, 1, 0, 3, 2])
+
     while 1:
         if c.connected:
             recevied = c.read()
-            c.send_data(encode_data(rand_data()))
+            dash.data = rand_data();
+            c.send_data(encode_data(dash.data))
         else:
             c.connect()
         time.sleep(1)
-
+        dash.master.update_idletasks()
+        dash.master.update()
     c.disconnect()
 
 def rand_data():
